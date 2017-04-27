da3ef6f4ab42973cd620e143fd335db527ae6f1c
Beniamin Sarkisyan <beniamin.sarkisyan@gmail.com>
2017-04-27 15:50:59 +0300

    Added obtain_top_versions command

---
+++ lib/fabricio/cli/build/obtain_top_versions.rb
@@ -0,0 +1,43 @@
+require 'thor'
+require 'terminal-table'
+require 'fabricio/client/client'
+require 'fabricio/models/organization'
+
+module Fabricio::CLI
+  class Application < Thor
+
+    desc 'obtain_all_builds [APPLICATION_ID] [START_TIMESTAMP] [END_TIMESTAMP] [USERNAME] [PASSWORD]', 'Obtains an array of top versions for a given application.'
+    def obtain_top_versions(application_id, start_timestamp, end_timestamp, username, password)
+      client = Fabricio::Client.new do |config|
+        config.username = username
+        config.password = password
+      end
+
+      builds = client.build.top_versions(application_id, start_timestamp, end_timestamp)
+
+      unless builds
+        puts "Can not find application with id: #{application_id}."
+        return
+      end
+
+      if builds.empty?
+        puts "Builds with application_id: #{application_id}, start_timestamp: #{start_timestamp}, end_timestamp: #{end_timestamp} not find."
+        return
+      end
+
+      table = Terminal::Table.new do |t|
+        rows = {}
+
+        builds.each_with_index do |build, index|
+          rows[index + 1] = build
+        end
+
+        t.rows = rows
+        t.style = {:all_separators => true}
+      end
+
+      puts table
+    end
+
+  end
+end
--- lib/fabricio/cli/cli.rb
+++ lib/fabricio/cli/cli.rb
@@ -2,6 +2,7 @@ require 'thor'
 require 'fabricio/cli/organization/obtain_organization_info_command'
 require 'fabricio/cli/build/obtain_all_builds'
 require 'fabricio/cli/build/obtain_build'
+require 'fabricio/cli/build/obtain_top_versions'
 
 module Fabricio::CLI
   class Application < Thor
