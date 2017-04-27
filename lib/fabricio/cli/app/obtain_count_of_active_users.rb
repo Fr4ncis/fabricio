2c6f41cd6ffa58d24e41784738a9d80eb0eda8fc
Beniamin Sarkisyan <beniamin.sarkisyan@gmail.com>
2017-04-27 17:18:17 +0300

    Added obtain_count_of_active_users command

---
+++ lib/fabricio/cli/app/obtain_count_of_active_users.rb
@@ -0,0 +1,40 @@
+require 'thor'
+require 'terminal-table'
+require 'fabricio/client/client'
+require 'fabricio/models/organization'
+
+module Fabricio::CLI
+  class Application < Thor
+
+    desc 'obtain_count_of_active_users [APPLICATION_ID] [USERNAME] [PASSWORD]', 'Obtains the count of active users at the current moment.'
+    def obtain_count_of_active_users(application_id, username, password)
+      client = Fabricio::Client.new do |config|
+        config.username = username
+        config.password = password
+      end
+
+      count_of_active_users = client.app.active_now(application_id)
+
+      unless count_of_active_users
+        puts "Application with id: #{application_id} not find."
+        return
+      end
+
+      print_table(count_of_active_users)
+    end
+
+    private
+
+    def print_table(count_of_active_users)
+      table = Terminal::Table.new do |t|
+        rows = {}
+        rows['Count of active users'] = count_of_active_users
+
+        t.rows = rows
+      end
+
+      puts table
+    end
+
+  end
+end
--- lib/fabricio/cli/cli.rb
+++ lib/fabricio/cli/cli.rb
@@ -5,6 +5,7 @@ require 'fabricio/cli/build/obtain_build'
 require 'fabricio/cli/build/obtain_top_versions'
 require 'fabricio/cli/app/obtain_all_applications'
 require 'fabricio/cli/app/obtain_application'
+require 'fabricio/cli/app/obtain_count_of_active_users'
 
 module Fabricio::CLI
   class Application < Thor
